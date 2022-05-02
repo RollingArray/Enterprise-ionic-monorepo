266612a8b78398e8f59fea12ac67905e64f09a91
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2022-05-01 18:56:14 -0700

    Remove node module

--- node_modules/@nrwl/devkit/ngcli-adapter.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * @category Ng CLI Adapter
- */
-export { wrapAngularDevkitSchematic, overrideCollectionResolutionForTesting, mockSchematicsForTesting, NxScopedHost, } from '@nrwl/tao/src/commands/ngcli-adapter';
--- node_modules/@nrwl/devkit/src/generators/update-ts-configs-to-js.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Tree } from '@nrwl/tao/src/shared/tree';
-export declare function updateTsConfigsToJs(tree: Tree, options: {
-    projectRoot: string;
-}): void;
